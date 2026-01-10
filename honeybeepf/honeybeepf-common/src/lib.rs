13540ad95319faa52531bb2d3918291ebc19f807
sammiee5311 <sammiee5311@gmail.com>
2026-01-10 13:31:40 +0900

    Add block io tracepoints

--- honeybeepf/honeybeepf-common/src/lib.rs
+++ honeybeepf/honeybeepf-common/src/lib.rs
@@ -1,11 +1,21 @@
 #![no_std]
 
 #[repr(C)]
-#[derive(Clone, Copy)]
-pub struct ConnectionEvent {
+#[derive(Clone, Copy, Default)]
+pub struct EventMetadata {
     pub pid: u32,
+    pub _pad: u32,
     pub cgroup_id: u64,
     pub timestamp: u64,
+}
+
+#[cfg(feature = "user")]
+unsafe impl aya::Pod for EventMetadata {}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct ConnectionEvent {
+    pub metadata: EventMetadata,
     pub dest_addr: u32,
     pub dest_port: u16,
     pub address_family: u16,
@@ -24,3 +34,19 @@ pub struct CommonConfig {
 
 #[cfg(feature = "user")]
 unsafe impl aya::Pod for CommonConfig {}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct BlockIoEvent {
+    pub metadata: EventMetadata,
+    pub dev: u32,
+    pub sector: u64,
+    pub nr_sector: u32,
+    pub bytes: u32,
+    pub rwbs: [u8; 8],
+    pub comm: [u8; 16],
+    pub event_type: u8, // 1: start, 2: done
+}
+
+#[cfg(feature = "user")]
+unsafe impl aya::Pod for BlockIoEvent {}
